b6ea0e2ff4dab85bbe24a47f1a81f1e4dd47c21e
Nicolas Giethlen <giethlen.nicolas@gmail.com>
2013-12-14 06:07:07 +0100

    Fix for glyphicon font path

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '44bdb2e34da299dc428e4b4008192179');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '636733611782d76e3048385d1827543b');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
